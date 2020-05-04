52fd64dd7cc4c10a9e31a339cf972e5e1d87b2a8
cblount <mathfan1980@gmail.com>
2020-05-03 20:54:14 -0400

    Completed 1st pass at the microservice implementation, the service now gets locations from AWS and cliff and returns them to the requestor.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import express from 'express';
 
-import * as ner from './ner-adapter.js';
+import * as neraws from './ner-aws-adapter.js';
+import * as nercc from './ner-cliff-clavin-adapter.js';
 // import * as c from './config/config';
 const app = express();
 const port = 8082; // default port to listen
+const createSummarizer = (results:any[],response:any, EXPECTED_RETURNS:number) =>
+  (source:string, result: any) => {
+    results.push({'source':source, 'result':result});
+    if (results.length >= EXPECTED_RETURNS){
+        response.send(results);
+    }
 
+};
     const testPhrase:string = 'In Brentwood, Tennessee, the co-owners of Zen Nails have taken their efforts one step further.';
 // define a route handler for the default home page
 app.get( '/', ( req, res ) => {
-    const awsret:any = ner.getEntities(testPhrase);
-    res.send( JSON.stringify(awsret));
+    const EXPECTED_RETURNS = 2;
+    const count  = 0;
+    const results = new Array();
+    const summarizer = createSummarizer(results, res, EXPECTED_RETURNS);
+    const ccret:any = nercc.getEntities(testPhrase, summarizer);
+    const awsret:any = neraws.getEntities(testPhrase, summarizer);
 } );
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port } ` + testPhrase);
+    console.log( `server started at http://localhost:${ port } :` + testPhrase);
 } );
--- src/ner-aws-adapter.ts
+++ src/ner-aws-adapter.ts
@@ -5,9 +5,12 @@ import { config } from './config/config'
 const c = config.dev;
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+type AggregatorReturn = (source: string, result:any)=>void;
+
 AWS.config.credentials = credentials;
 AWS.config.update({region:'us-east-1'});
 const  comprehend = new AWS.Comprehend();
+let callback:AggregatorReturn;
 
 /* getEntities get the named entities
  * @Params
@@ -16,21 +19,31 @@ const  comprehend = new AWS.Comprehend();
  *    the the ner object
  */
 const  handleAWSComprehendReturn = async(err:any, data:any) => {
+    const locations = new Array();
     if (err){ console.log(err, err.stack); // an error occurred
+
+      console.log('aws error');
         return {};
     }else   {
         console.log(data);
         // successful response
-        return data
+        if (data !== 'undefined' && data.Entities !== 'undefined'){
+            for (const entry of data.Entities) {
+                if(entry.Type==='LOCATION'){
+                    locations.push({'name':entry.Text, 'confScore': entry.Score})
+                }
+            }
+        }
+        callback('aws-comprehend',locations);
 
     }
 }
 
-export function getEntities( text: string ): any{
+export function getEntities( text: string  , _callback: AggregatorReturn): any{
     const  params = {
         LanguageCode: 'en', /* required */
         Text: text /* required */
       };
-
-    comprehend.detectEntities(params, handleAWSComprehendReturn);
+      callback = _callback;
+ comprehend.detectEntities(params, handleAWSComprehendReturn);
 }
